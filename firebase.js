c562682a3f85615971b2adfe57c877432402668f
Mc Phy P. Cabanes <73990197+Peemakpee@users.noreply.github.com>
2024-10-31 08:23:27 +0800

    removed credentials

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqwGohMbtJUFenWu7bdmtnWt653BYkAdU",
-    authDomain: "ccetracker.firebaseapp.com",
-    projectId: "ccetracker",
-    storageBucket: "ccetracker.appspot.com",
-    messagingSenderId: "699760839322",
-    appId: "1:699760839322:web:4819bdb9cc2737494bed23",
-    measurementId: "G-X0C969TTDB"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
